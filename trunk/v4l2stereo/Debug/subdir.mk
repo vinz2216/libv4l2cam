9e937b5769f481d035fee5b1ef8aab4e4cb1211f
fuzzgun <fuzzgun@d48d6e40-53f7-11de-9eea-55519c9c5eb7>
2009-10-26 21:06:39 +0000

    eclipse project changes for easy compilation on Ubuntu Karmic

--- trunk/v4l2stereo/Debug/subdir.mk
+++ trunk/v4l2stereo/Debug/subdir.mk
@@ -38,7 +38,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/opt/opencv/include/opencv -O0 -g3 -Wall -c -fmessage-length=0 -lcam -lcv -lcxcore -lcvaux -lhighgui -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	g++ -I/usr/include/opencv -O0 -g3 -Wall -c -fmessage-length=0 -lcam -lcv -lcxcore -lcvaux -lhighgui -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
