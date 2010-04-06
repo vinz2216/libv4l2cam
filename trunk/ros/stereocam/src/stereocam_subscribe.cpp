08d9e01ebf8e20ef2f46e4f88919c00ee692b869
fuzzgun <fuzzgun@d48d6e40-53f7-11de-9eea-55519c9c5eb7>
2010-04-06 21:08:52 +0000

    Ability to set stereo camera parameters as a ROS service

--- trunk/ros/stereocam/src/stereocam_subscribe.cpp
+++ trunk/ros/stereocam/src/stereocam_subscribe.cpp
@@ -17,17 +17,72 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <cstdlib>
+#include <cv.h>
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
+#include "stereocam/stereocam_params.h"
+#include "cv_bridge/CvBridge.h"
 
-void leftImageCallback(const sensor_msgs::ImageConstPtr& img)
+IplImage* left = NULL;
+IplImage* right = NULL;
+sensor_msgs::CvBridge bridge_;
+ros::ServiceClient client;
+
+void process_images(
+    IplImage* left_image,
+    IplImage* right_image)
 {
-  ROS_INFO("Received left image");
 }
 
-void rightImageCallback(const sensor_msgs::ImageConstPtr& img)
+void leftImageCallback(const sensor_msgs::ImageConstPtr& ptr)
+{
+  ROS_INFO("Received left image");
+
+  try
+  {
+      left = bridge_.imgMsgToCv(ptr, "bgr8");
+  }
+  catch (sensor_msgs::CvBridgeException error)
+  {
+      ROS_ERROR("Error converting left image to IplImage");
+  }
+}
+
+void rightImageCallback(const sensor_msgs::ImageConstPtr& ptr)
 {
   ROS_INFO("Received right image");
+
+  try
+  {
+      right = bridge_.imgMsgToCv(ptr, "bgr8");
+      if (left != NULL) process_images(left, right);
+  }
+  catch (sensor_msgs::CvBridgeException error)
+  {
+      ROS_ERROR("Error converting right image to IplImage");
+  }
+}
+
+void set_stereo_camera_params(
+    std::string left_device,
+    std::string right_device,
+    int width,
+    int height,
+    int fps)
+{
+  stereocam::stereocam_params srv;
+  srv.request.left_device = left_device;
+  srv.request.right_device = right_device;
+  srv.request.width = width;
+  srv.request.height = height;
+  srv.request.fps = fps;
+  if (client.call(srv)) {
+      ROS_INFO("Changed stereo camera parameters: %d", (int)srv.response.ack);
+  }
+  else {
+      ROS_ERROR("Failed to call service add_two_ints");
+  }
 }
 
 int main(int argc, char** argv)
@@ -36,6 +91,9 @@ int main(int argc, char** argv)
   ros::NodeHandle n;
   ros::Subscriber left_sub = n.subscribe("stereo/left/image_raw", 30, leftImageCallback);
   ros::Subscriber right_sub = n.subscribe("stereo/right/image_raw", 30, rightImageCallback);
+  client = n.serviceClient<stereocam::stereocam_params>("stereocam_params");
+  set_stereo_camera_params("/dev/video1","/dev/video0",320,240,30);
+
   ros::spin();
 }
 
