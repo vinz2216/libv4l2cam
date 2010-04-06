2db56b60edf6fa3f97720aa2863030567ad61529
fuzzgun <fuzzgun@d48d6e40-53f7-11de-9eea-55519c9c5eb7>
2010-04-06 21:08:52 +0000

    Ability to set stereo camera parameters as a ROS service

--- ros/stereocam/src/stereocam_broadcast.cpp
+++ ros/stereocam/src/stereocam_broadcast.cpp
@@ -27,18 +27,96 @@
 #include <highgui.h>
 #include <stdio.h>
 
+#include "stereocam/stereocam_params.h"
 #include "libcam.h"
 #include "libcam.cpp"
 
+std::string dev0 = "/dev/video1";
+std::string dev1 = "/dev/video0";
+
+int fps = 30;
+int ww = 320;
+int hh = 240;
+Camera *c = NULL;
+Camera *c2 = NULL;
+sensor_msgs::Image left;
+sensor_msgs::Image right;
+
+/*!
+ * \brief stop the stereo camera
+ * \param left_camera left camera object
+ * \param right_camera right camera object
+ */
+void stop_cameras(
+  Camera *&left_camera,
+  Camera *&right_camera)
+{
+    if (left_camera != NULL) {
+        delete left_camera;
+        delete right_camera;
+        left_camera = NULL;
+        right_camera = NULL;
+    }
+}
+
+/*!
+ * \brief start the stereo camera
+ * \param left_camera left camera object
+ * \param right_camera right camera object
+ * \param left_img left image object
+ * \param right_img right image object
+ * \param left_device left camera device, eg. "/dev/video1"
+ * \param right_device right camera device, eg. "/dev/video0"
+ * \param width desired image width
+ * \param height desired image height
+ * \param fps desired frames per second
+ */
+void start_cameras(
+  Camera *&left_camera,
+  Camera *&right_camera,
+  sensor_msgs::Image &left_img,
+  sensor_msgs::Image &right_img,
+  std::string left_device,
+  std::string right_device,
+  int width,
+  int height,
+  int fps)
+{
+    stop_cameras(left_camera, right_camera);
+
+    left_camera = new Camera(left_device.c_str(), width, height, fps);
+    right_camera = new Camera(right_device.c_str(), width, height, fps);
+
+    left_img.width  = width;
+    left_img.height = height;
+    left_img.step = width * 3;
+    left_img.encoding = "bgr8";
+    left_img.set_data_size(width*height*3);
+
+    right_img.width  = width;
+    right_img.height = height;
+    right_img.step = width * 3;
+    right_img.encoding = "bgr8";
+    right_img.set_data_size(width*height*3);
+}
+
+/*!
+ * \brief service requests camera parameters to be changed
+ * \param req requested parameters
+ * \param res returned parameters
+ */ 
+bool request_params(
+  stereocam::stereocam_params::Request &req,
+  stereocam::stereocam_params::Response &res)
+{
+    ROS_INFO("Resolution: %dx%d", (int)req.width, (int)req.height);
+    start_cameras(c,c2,left,right,req.left_device,req.right_device,req.width,req.height,req.fps);
+    res.ack = 1;
+    return(true);
+} 
+
 int main(int argc, char** argv)
 {
-  std::string dev0 = "/dev/video1";
-  std::string dev1 = "/dev/video0";
-
-  const int fps = 30;
-  const int ww = 320;
-  const int hh = 240;
-
   ros::init(argc, argv, "stereocam_broadcast");
   ros::NodeHandle n;
   ros::Publisher left_pub = n.advertise<sensor_msgs::Image>("stereo/left/image_raw", fps);
@@ -46,17 +124,13 @@ int main(int argc, char** argv)
   ros::Rate loop_rate(20);
   int count = 0;
 
-  Camera c(dev0.c_str(), ww, hh, fps);
-  Camera c2(dev1.c_str(), ww, hh, fps);
+  start_cameras(c,c2,left,right,dev0,dev1,ww,hh,fps);
 
   IplImage *l=cvCreateImage(cvSize(ww, hh), 8, 3);
   IplImage *r=cvCreateImage(cvSize(ww, hh), 8, 3);
 
   unsigned char *l_=(unsigned char *)l->imageData;
   unsigned char *r_=(unsigned char *)r->imageData;
-
-  sensor_msgs::Image left;
-  sensor_msgs::Image right;
 
   left.width  = ww;
   left.height = hh;
@@ -70,14 +144,17 @@ int main(int argc, char** argv)
   right.encoding = "bgr8";
   right.set_data_size(ww*hh*3);
 
+  // start service which can be sued to change camera parameters
+  ros::ServiceServer service_params = n.advertiseService("stereocam_params", request_params);
+
   while (ros::ok())
   {
     // Read image data
-    while(c.Get()==0 || c2.Get()==0) usleep(100);
+    while(c->Get()==0 || c2->Get()==0) usleep(100);
 
     // Convert to IplImage
-    c.toIplImage(l);
-    c2.toIplImage(r);
+    c->toIplImage(l);
+    c2->toIplImage(r);
 
     // Convert to sensor_msgs::Image
     memcpy ((void*)(&left.data[0]), (void*)l_, ww*hh*3);
@@ -95,5 +172,6 @@ int main(int argc, char** argv)
 
   cvReleaseImage(&l);
   cvReleaseImage(&r);
+  stop_cameras(c,c2);
 }
 
