a95cf05c338192969416db06cb1c3d20dcb5847d
fuzzgun <fuzzgun@d48d6e40-53f7-11de-9eea-55519c9c5eb7>
2010-12-23 16:18:51 +0000

    Simplified ROS driver

--- ros/setup.sh
+++
@@ -1,7 +0,0 @@
-export ROS_ROOT=/home/motters/ros/ros
-export PATH=$ROS_ROOT/bin:$PATH
-export PYTHONPATH=$ROS_ROOT/core/roslib/src:$PYTHONPATH
-if [ ! "$ROS_MASTER_URI" ] ; then export ROS_MASTER_URI=http://localhost:11311 ; fi
-export ROS_PACKAGE_PATH=/home/motters/develop/libv4l2cam/ros:/home/motters/ros/pkgs
-
-source $ROS_ROOT/tools/rosbash/rosbash
