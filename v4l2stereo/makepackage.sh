5e623497b375e89e45e3a172e5eb3fde287767a2
fuzzgun <fuzzgun@d48d6e40-53f7-11de-9eea-55519c9c5eb7>
2011-11-13 13:05:36 +0000

    Added missing stsreocam launch file

--- v4l2stereo/makepackage.sh
+++ v4l2stereo/makepackage.sh
@@ -15,3 +15,4 @@ cp deb/DEBIAN/install ~/temp/v4l2stereo/deb/DEBIAN
 sudo chmod -R 0755 ~/temp/v4l2stereo/deb/usr
 dpkg -b ~/temp/v4l2stereo/deb v4l2stereo.deb
 rm -rf ~/temp/v4l2stereo
+echo You may also wish to upload a screenshot for this version to http://screenshots.debian.net/upload
